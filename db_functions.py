72b347d5a792631aef84d6646ecdcba92f5a9c8c
Samu <Samu.kirjonen@gmail.com>
2024-09-25 15:23:45 +0300

    Add database connection function

--- db_functions.py
+++ db_functions.py
@@ -1,3 +1,14 @@
 import os
 from dotenv import load_dotenv
 load_dotenv()
+
+import mysql.connector
+
+def get_db_connection():
+    conn = mysql.connector.connect(
+    host='localhost',
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    database=os.getenv("DB_NAME"),
+    )
+    return conn
