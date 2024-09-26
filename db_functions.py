2cb73461bc075d657950cbe3a1cc2622857446a4
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-09-26 16:38:31 +0300

    Comment added

--- db_functions.py
+++ db_functions.py
@@ -1,20 +1,23 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
 
+# Load environment variables from .env file
+load_dotenv()
 
 import mysql.connector
 
+
 def get_db_connection():
     conn = mysql.connector.connect(
-    host='127.0.0.1',
-    port=3306,
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME"),
-    autocommit=True
+        host='127.0.0.1',
+        port=3306,
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        autocommit=True
     )
     return conn
+
 
 def db_query(sql):
     conn = get_db_connection()
@@ -22,5 +25,3 @@ def db_query(sql):
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
-
-
