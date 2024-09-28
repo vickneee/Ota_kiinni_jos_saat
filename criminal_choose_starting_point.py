210ebc7069350eadc7b26d23283fd3f5d40d54ca
Alessa <alessa.pentinmikko@hotmail.com>
2024-09-28 16:28:34 +0300

    update criminal_choose_starting_point.py

--- criminal_choose_starting_point.py
+++ criminal_choose_starting_point.py
@@ -1,11 +1,10 @@
 from airport_table import print_airports, get_airports
-from db_functions import db_insert
 from player_table import insert_player
 
 
 # Karkurin valitsee oman aloituspaikan kaikista mahdollisista lentokentistä ja sijainneista
 # Choose starting point for the criminal from all possible airports and locations
-def criminal_choose_starting_point():
+def criminal_choose_starting_point(name):
     # Karkuri valitsee aloituspaikan
     print("Karkuri valitsee aloituspaikan")
     print_airports(get_airports())
@@ -15,19 +14,10 @@ def criminal_choose_starting_point():
     location = airports[selected_icao]
     print(f"Karkuri on valinnut aloituspaikakseen lentokentän numero {choose}.")
     print(f"""Lentokenttä: {location['name']}, Maa: {location['country']}, Sijainti: ({location['latitude']}, {location['longitude']})""")
-    # Karkurin aloituspaikan tallentaminen tietokantaan
-    # Insert the criminal's starting point into the database
-    name = "Karkuri"
-    type = 0
-    icao_code = selected_icao  # ICAO code of the airport where the criminal starts the game
-    # Call function insert_player from player_table.py
-    insert_player(name, type, icao_code)
-    # SQL query
-    sql = f"""INSERT INTO player (screen_name, type, location) 
-    VALUES ('{name}', {type}, '{icao_code}')"""
-    # Execute SQL query
-    add = db_insert(sql)
+    #insert the player into the database
+    add = insert_player(name, 0, selected_icao)
 
-    return choose
+
+    return add
 
 
