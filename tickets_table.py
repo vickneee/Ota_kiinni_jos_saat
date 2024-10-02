2c7c732b5aa1d71162599e7e42dd9c43e3c70b91
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-02 14:48:07 +0300

    Lipun delete update again

--- tickets_table.py
+++ tickets_table.py
@@ -6,6 +6,7 @@ def delete_ticket(ticket_type, player_id):
     sql = f"""DELETE FROM tickets 
     WHERE ticket_type = '{ticket_type}' and  player_id = '{player_id}' LIMIT 1"""
     delete = db_delete(sql)
+
     return delete
 
 def insert_tickets(player_id, ticket_type):
