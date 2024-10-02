0a0dba11eeb4d01b13967804b06412afd2fecf1b
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-10-02 13:32:40 +0300

    Player move available tickets added

--- player_move.py
+++ player_move.py
@@ -1,5 +1,6 @@
 from airport_table import get_airports, print_airports, get_recommended_airports, print_recommended_airports
-from player_management import update_location
+from player_management import update_location, get_players_info
+from tickets_table import player_tickets
 
 
 def player_move(name):
@@ -12,12 +13,21 @@ def player_move(name):
     # Get recommended airports for the player
     recommended_airports = get_recommended_airports(name)
 
+    get_players_info(name)
+    player_id = get_players_info(name).get('id')
+    # print(player_id)
+    available_tickets = player_tickets(player_id)
+    # print(available_tickets)
+    print("Sinulla on seuraavat lentoliput:")
+    for key, value in available_tickets.items():
+        print(f"{key}: {value} kpl")
+
     # Print recommended airports using the name parameter (sorted from farthest to nearest)
-    print("Suositellut lentokentät:")
     print_recommended_airports(name)  # This function handles sorting and printing
 
     # Extract keys for indexing from recommended_airports
     recommended_keys = list(recommended_airports.keys())  # Get keys directly from the dictionary
+    print(recommended_keys)
 
     # Ask the player to select an airport
     while True:
@@ -29,7 +39,7 @@ def player_move(name):
                 selected_airport = recommended_airports[selected_key]  # Get the airport details using the key
 
                 # Print exact airport details
-                print(f"""Valitsit lentokentän: {selected_airport['country']} : {selected_airport['name']} - {selected_airport['distance']:.2f} km""")
+                print(f"""Valitsit lentokentän: {selected_airport['country']} : {selected_airport['name']} ja käytit """)
                 break  # Exit loop if input is valid
 
             else:
@@ -41,4 +51,4 @@ def player_move(name):
 
 
 # Example call to the function
-player_move("Janne")
+player_move("Kimmo")
