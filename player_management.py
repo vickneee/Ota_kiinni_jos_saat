4d26a4472e9bfa25ce897cabb15a14fb513cb09e
donit <donitrivedi@gmail.com>
2024-10-04 13:56:26 +0300

    colors_for_text_2 #Virhesyötteille puninen väli

--- player_management.py
+++ player_management.py
@@ -138,7 +138,7 @@ def criminal_choose_starting_point(name, is_computer=0):
         print_airports(get_airports())
         choose = int(input("Valitse aloituspaikka (1-21): "))
         while choose < 1 or choose > 21:
-            print("Virheellinen syöte. Valitse aloituspaikka (1-21): ")
+            print(colored("Virheellinen syöte. Valitse aloituspaikka (1-21): ","red"))
             choose = int(input("Valitse aloituspaikka (1-21): "))
 
         selected_icao = list(airports.keys())[choose - 1]
