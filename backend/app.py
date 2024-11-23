2fb1b5c580dfb3aa07fda462ec71f640e4d75f78
Alessa <alessa.pentinmikko@hotmail.com>
2024-11-23 14:54:40 +0200

    update mapscript

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,18 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+
 import os
+from dotenv import load_dotenv
+load_dotenv()
 app = Flask(__name__)
-
-
+CORS(app)
 @app.route('/api/env')
 def get_env():
     return jsonify({
         'MAP_KEY': os.getenv('MAP_KEY')
     })
+
+
 
 if __name__ == '__main__':
     app.run(use_reloader=True, host='127.0.0.1', port=3000)
