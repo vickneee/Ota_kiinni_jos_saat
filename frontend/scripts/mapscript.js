c5d49604fff7a9e0bd3f34d851676b5bd7f48e4e
Victoria Vavulina <93821265+vickneee@users.noreply.github.com>
2024-11-24 06:45:25 +0200

    Select option script added

--- frontend/scripts/mapscript.js
+++ frontend/scripts/mapscript.js
@@ -2,9 +2,9 @@
 let map;
 
 async function fetchEnv() {
- const response = await fetch('http://127.0.0.1:3000/api/env');
- const env = await response.json();
- return env;
+  const response = await fetch('http://127.0.0.1:3000/api/env');
+  const env = await response.json();
+  return env;
 }
 
 async function loadGoogleMapsAPI(apiKey) {
@@ -21,32 +21,39 @@ async function loadGoogleMapsAPI(apiKey) {
 
 async function initMap() {
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = {lat: -25.344, lng: 131.031};
   // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const {Map} = await google.maps.importLibrary('maps');
+  const {AdvancedMarkerElement} = await google.maps.importLibrary('marker');
 
   // The map, centered at Uluru
-  map = new Map(document.getElementById("map"), {
+  map = new Map(document.getElementById('map'), {
     zoom: 4,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: 'DEMO_MAP_ID',
   });
 
   // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: 'Uluru',
   });
 }
 
 fetchEnv().then(env => {
- const mapKey = env.MAP_KEY;
- loadGoogleMapsAPI(mapKey).then(() => {
-   initMap();
- }).catch(error => {
-   console.error(error);
- });
+  const mapKey = env.MAP_KEY;
+  loadGoogleMapsAPI(mapKey).then(() => {
+    initMap();
+  }).catch(error => {
+    console.error(error);
+  });
+});
+
+document.getElementById('menu').addEventListener('change', function() {
+  const value = this.value;
+  if (value) {
+    window.location.href = value;
+  }
 });
