e737d96445d4caaf0a191d6df1b60b73897766f7
Samu <Samu.kirjonen@gmail.com>
2024-11-30 18:57:40 +0200

    .

--- frontend/scripts/choosestartscript.js
+++ frontend/scripts/choosestartscript.js
@@ -1,16 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const playerInfo = fetch('/api/player-info?screen_name=player_name')  // Replace 'player_name' with the actual player name
-        .then(response => response.json())
-        .then(playerInfo => {
-            const startBanner = document.getElementById('start-banner');
-            const playBanner = document.getElementById('play-banner');
-
-            if (playerInfo.type === 0 && !playerInfo.location) {
-                startBanner.style.display = 'table';
-                playBanner.style.display = 'none';
-            } else {
-                startBanner.style.display = 'none';
-                playBanner.style.display = 'table';
-            }
-        });
+    const startBanner = document.getElementById('start-banner');
+    const playBanner = document.getElementById('play-banner');
+    const players = playerData();
+    const firstPlayer = players ? players[0] : null;
+    startBanner.textContent = `${firstPlayer.name} choose starting position`;
+        if (firstPlayer && firstPlayer.type === 0) {
+            startBanner.style.display = 'table';
+            playBanner.style.display = 'none';
+    } else {
+        startBanner.style.display = 'none';
+        playBanner.style.display = 'table';
+    }
 });
